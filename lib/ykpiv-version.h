58757487a36078f7c5b80ba2ac36831492f98275
Alessio Di Mauro <alessio@yubico.com>
2015-11-11 18:57:44 +0100

    Imported Upstream version 1.1.1

--- lib/ykpiv-version.h
+++ lib/ykpiv-version.h
@@ -43,7 +43,7 @@ extern "C"
 	 * version number.  Used together with ykneomgr_check_version() to verify
 	 * header file and run-time library consistency.
 	 */
-#define YKPIV_VERSION_STRING "1.1.0"
+#define YKPIV_VERSION_STRING "1.1.1"
 
 	/**
 	 * YKPIV_VERSION_NUMBER
@@ -53,7 +53,7 @@ extern "C"
 	 * this symbol will have the value 0x01020300.  The last two digits
 	 * are only used between public releases, and will otherwise be 00.
 	 */
-#define YKPIV_VERSION_NUMBER 0x010100
+#define YKPIV_VERSION_NUMBER 0x010101
 
 	/**
 	 * YKPIV_VERSION_MAJOR
@@ -80,7 +80,7 @@ extern "C"
 	 * level of the header file version number.  For example, when the
 	 * header version is 1.2.3 this symbol will be 3.
 	 */
-#define YKPIV_VERSION_PATCH 0
+#define YKPIV_VERSION_PATCH 1
 
 	const char *ykpiv_check_version (const char *req_version);
 
